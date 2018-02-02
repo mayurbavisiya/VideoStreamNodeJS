25b5f627bc17693611508c4bc92cf8501a85ef3c
Mayur <mayur.bavisiya@gmail.com>
2018-02-02 11:05:19 +0400

    Port chage runtime

--- video-stream-sample/server.js
+++ video-stream-sample/server.js
@@ -43,6 +43,8 @@ app.get('/video', function(req, res) {
   }
 })
 
-app.listen(3000, function () {
-  console.log('Listening on port 3000!')
-})
+var port = process.env.PORT || 8000
+app.listen(port, function() {
+    console.log("App is running on port " + port);
+});
+
